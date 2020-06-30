5bb79eb3a9dfcb529a147867e8b4359f97621c37
Carlos <carlos.fletes@marvelsa.com>
2020-06-30 16:38:29 -0500

    1.0.0

---
+++ README.rst
@@ -0,0 +1,13 @@
+=======================
+ Quick online shopping
+=======================
+
+Description: https://apps.odoo.com/apps/modules/11.0/website_sale_add_to_cart/
+
+Maintainers
+-----------
+* `IT-Projects LLC <https://it-projects.info>`__
+
+    This module is not maintained since Odoo 12.0
+
+Tested on Odoo 11.0 c7171795f891335e8a8b6d5a6b796c28cea77fea
---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Quick online shopping",
+    'version': "12.0.1.0.2",
+    'author': "Marvelsa",
+    'license': "LGPL-3",  # MIT
+    'category': "eCommerce",
+    'website': "https://www.marvelsa.com",
+    'depends': ['base','website','website_sale'],
+    'images': ['images/shop.png'],
+    'data': ['views/website_sale_add_to_cart_views.xml'],
+    'installable': True,
+}
